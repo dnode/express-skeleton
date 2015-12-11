99069df356a471c80e68cd1e218daad5b8a9c888
Christoph Herrmann <developer@dragonprojects.de>
2015-12-11 03:23:58 +0100

    express-skeleton

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Dependency Status](https://david-dm.org/dnode/express-skeleton.svg)](https://david-dm.org/dnode/express-skeleton)
+[![devDependency Status](https://david-dm.org/dnode/express-skeleton/dev-status.svg)](https://david-dm.org/dnode/express-skeleton#info=devDependencies)
---
+++ src/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('dnewrelic');
+
+let app = require('dexpress');
+app.use(require('dheaders'));
+const dhttpAuth = require('dhttp-auth');
+if (dhttpAuth) {
+  app.use(dhttpAuth);
+}
+app.use(require('./middlewares/example.js')());
+require('dcontrollers')(app, [
+  require('./controllers/example.js')()
+]);
