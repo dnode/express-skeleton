1b20a779d5746794de64927f3f648c92e7e2cdfd
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-04-18 12:47:40 +0200

    Always use www as document root for static files

--- src/app.js
+++ src/app.js
@@ -12,4 +12,4 @@ app.use(require('./middlewares/example.js')());
 require('dcontrollers')(app, [
   require('./controllers/example.js')()
 ]);
-app.use(app.express.static('web'));
+app.use(app.express.static('www'));
