62c6d71dd3ddbab92823f1e584ef17a53fde40b8
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-05-04 12:43:28 +0200

    Simplify by using dwebserver as base

--- src/app.js
+++ src/app.js
@@ -1,15 +1,7 @@
 'use strict';
 
-require('dnewrelic');
-
-let app = require('dexpress');
-app.use(require('dheaders'));
-const dhttpAuth = require('dhttp-auth');
-if (dhttpAuth) {
-  app.use(dhttpAuth);
-}
+const app = require('dwebserver');
 app.use(require('./middlewares/example.js')());
 require('dcontrollers')(app, [
   require('./controllers/example.js')()
 ]);
-app.use(app.express.static('www'));
